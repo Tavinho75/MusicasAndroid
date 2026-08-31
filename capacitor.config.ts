4f85e4f16c5713a9bc464630cd5818944840335a
Tavinho75 <128919525+Tavinho75@users.noreply.github.com>
2026-08-31 17:38:26 -0300

    Phase 1: restore capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.downloaderandroid',
+  appName: 'MusicasAndroid',
+  webDir: 'frontend'
+};
+
+export default config;
